48e2f5608ff474b2e9537c15253d57399a1c4296
faraji-fuji <farajifuji@gmail.com>
2023-04-17 14:33:11 +0300

    firebase authentication

---
+++ static/app-setup.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_e8ZFyOQpqRopnUjpBj6P2ftUM65Paaw",
+  authDomain: "calendar-383306.firebaseapp.com",
+  projectId: "calendar-383306",
+  storageBucket: "calendar-383306.appspot.com",
+  messagingSenderId: "640631341702",
+  appId: "1:640631341702:web:a02c4abba8874776bc8707",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
